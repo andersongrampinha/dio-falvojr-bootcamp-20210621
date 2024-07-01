9f5ce4a9fbbca17bca92f489a1d332b8b8e639f8
Anderson Grampinha <andersongrampinha@gmail.com>
2024-07-01 15:03:19 -0300

    Conteúdo da mentoria dio-live-20210621

---
+++ src/dio/dominio/Mentoria.java
@@ -0,0 +1,22 @@
+package dio.dominio;
+
+import java.time.LocalDateTime;
+
+public class Mentoria extends Conteudo {
+	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO;
+	}
+
+	private LocalDateTime data;
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+}
